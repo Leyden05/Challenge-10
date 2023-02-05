b808ed565040d0b489ee52726b883c148c67d3db
leyden <fusionsoccer05@gmail.com>
2023-02-04 18:22:27 -0600

    almost ready for final push -- just need to add tests

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const Employee = require('./employee');
-const engineer = require('./lib/engineer');
-const manager = require('./lib/manager');
-const intern = require('./lib/intern');
+const Employee = require('./lib/employee');
+const {Engineer} = require('./lib/engineer');
+const {Manager} = require('./lib/manager');
+const {Intern} = require('./lib/intern');
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateTemplate = require('./src/buildTemplate');
+
+let allEmployees = [];
 
 const questions = [
     {
@@ -29,28 +32,67 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'Describe the contributions outside parties made:',
-        name: 'school'
+        message: 'What is the name of your current school?',
+        name: 'school',
+        when: (answers) => answers.position === 'Intern'
     },
     {
         type: 'input',
         message: 'What is your office number?',
-        name: 'officenum'
+        name: 'officenum',
+        when: (answers) => answers.position === 'Manager'
     },
     {
         type: 'input',
-        message: 'Describe the contributions outside parties made:',
-        name: 'github'
+        message: 'What is your github username?',
+        name: 'github',
+        when: (answers) => answers.position === 'Engineer'
     },
     
 ];
+
+function addEmployee() {
+    inquirer
+    .prompt([{
+        type: 'list',
+        message: 'Do you want to add another employee',
+        name: 'add',
+        choices: [`I'm ready to build a profile for my team`, `I'd like to add another employee`]
+    }
+    ])
+    .then(answers => {
+        if (answers.add === `I'm ready to build a profile for my team`) {
+            console.log(allEmployees);
+            const template = generateTemplate(allEmployees);
+                fs.writeFile('./dist/index.html', template, (err) =>
+                err ? console.error(err) : console.log('Success!')
+              );
+        } else {
+            // runs init again
+            init();
+        }
+    })
+}
 
 function init() {
     inquirer
     .prompt(questions)
     .then(answers => {
+    if (answers.position === 'Engineer') {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+        allEmployees.push(engineer);
+    } else if (answers.position === 'Intern') {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+        allEmployees.push(intern);
+    } else {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officenum)
+       allEmployees.push(manager);
+    }
+
+    addEmployee();
 
     })
 }
+
 
 init();
